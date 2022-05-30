4e72d5165cdc0a8d7f798c5c15c767b44dd21c0b
Lucas Emanuel Oliveira de Carvalho <lucas.emanuel.carvalho@outlook.com>
2022-05-30 16:16:47 -0300

    Add files via upload

---
+++ dados_2.js
@@ -0,0 +1,14 @@
+// Cria-se a variável (let ou var) e depois o identificador, o ";" é opcional.
+let preco = 19.90;
+let desconto = 0.4;
+
+console.log(preco*0.6);
+console.log(preco*(1 - desconto));
+
+let nome = "Caderno";
+let categoria = "Papelaria";
+
+console.log("Produto: " + nome 
+   +", Categoria: " + categoria
+    + ", Preço: " + preco
+    + ", Desconto: " + desconto);
