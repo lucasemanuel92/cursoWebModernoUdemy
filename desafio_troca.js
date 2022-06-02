25d44e1d30309b55a5b093d5a363d3655e6d04e9
Lucas Emanuel Oliveira de Carvalho <lucas.emanuel.carvalho@outlook.com>
2022-06-02 15:14:43 -0300

    Add files via upload

---
+++ desafio_troca.js
@@ -0,0 +1,12 @@
+let a = 7;
+let b = 94;
+
+// depois da troca a deve ser 97 e b valer 7
+
+a = 94; 
+b = 7;
+
+
+// [a, b] = [b, a]; Essa é a forma mais rápida de trocar o valor de variáveis etnre si
+console.log(a);
+console.log(b);
