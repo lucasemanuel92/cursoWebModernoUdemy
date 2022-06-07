adc52f58f5f3b96b3aad8bedfd9be151ba840aa2
Lucas Emanuel Oliveira de Carvalho <lucas.emanuel.carvalho@outlook.com>
2022-06-07 11:29:39 -0300

    Add files via upload

---
+++ fundamentos_geral/dados_1.js
@@ -0,0 +1,16 @@
+var nome = "Caneta";
+var quantidade = 10;
+var preco = 6.4;
+var imposto = 1.5
+//Var // Atualmente se usa o termo "let"
+
+console.log("Caneta");
+console.log(quantidade);
+console.log(preco);
+console.log(imposto);
+console.log(preco + imposto);
+
+nome = "Caneta Bic"; // Alterando o variável incial
+// Não é necessário usar o termo var no inicio da variável, mas isso pode gerar problemas
+
+console.log(nome);
---
+++ fundamentos_geral/dados_3.js
@@ -0,0 +1,12 @@
+let idade = 30;
+
+console.log(typeof -30); // typeof diz o tipo de variável
+console.log(typeof idade);
+
+let salario = 456.98;
+console.log(typeof salario);
+
+let estaChovendo = true; // ou false. boolean
+console.log(typeof estaChovendo);
+
+console.log(typeof "Teste"); // String pode ser definida com aspas duplas ou simples
