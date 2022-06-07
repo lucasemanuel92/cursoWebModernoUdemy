543b21d4a0b2a0c015e2004a66c744f7ae6c369b
Lucas Emanuel Oliveira de Carvalho <lucas.emanuel.carvalho@outlook.com>
2022-06-07 12:54:11 -0300

    Add files via upload

---
+++ fundamentos_javascript/numerosAlgunsCuidados.js
@@ -0,0 +1,12 @@
+console.log( 7 / 0) // Retorna o infinito
+console.log('10' / 2) // O sistema verifica se a string pode ser convertida em numérico
+
+// javascritp usa o modelo americano, logo a "," tem que ser mudada para "."
+
+console.log(0.1 + .7) // Tem problema com .flutuente; Não precisa usar o 0. pode só usar o .numero 
+
+/* console.log(10.toString()) para alterar para string é necessário usar a variavel declarada ou declarar
+a variável e depois alterar
+*/
+
+console.log((10.3).toString())
