88aa4560b38c8744cae176b86d62412ecbddd217
Lucas Emanuel Oliveira de Carvalho <lucas.emanuel.carvalho@outlook.com>
2022-08-01 17:11:36 -0300

    Add files via upload

---
+++ fundamentos_javascript/usandoVarEmLoop1.js
@@ -0,0 +1,8 @@
+for (var i = 0; i < 10; i++) {
+    console.log(i)
+}
+
+console.log('i = ', i)
+
+// o var não tem escopo de bloco
+// O valor de i fora do bloco é o valor que faz com que o i saia do laço de repetição
